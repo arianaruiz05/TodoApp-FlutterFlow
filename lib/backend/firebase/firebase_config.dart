2b4a6d226f5cd0ff5d45884255673e2764f8dfcd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-07 18:59:26 +0000

    TodoApp-FlutterFlow-Push

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCQwFAB4182f1wj5549Hd-37EWkuO2xgTE",
+            authDomain: "todo-xt4jad.firebaseapp.com",
+            projectId: "todo-xt4jad",
+            storageBucket: "todo-xt4jad.firebasestorage.app",
+            messagingSenderId: "1060701750591",
+            appId: "1:1060701750591:web:c80e12aeb8a9461a3abf8a"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
